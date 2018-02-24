5ba869a50ec90982981071da1c4fb793a5b5064e
Dan Munckton <dan@cultivatehq.com>
2018-02-24 20:06:29 +0000

    Demonstrate shadowing in blocks

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -19,3 +19,39 @@ fn using_local_variables() {
     assert_eq!(payment.amount, 1000);
     assert_eq!(payment.currency, "EUR");
 }
+
+#[test]
+fn shadowing_to_override_variables_context() {
+    let restaurant = Restaurant {
+        name: "We make Pizza!",
+        iban: "GB99ABCD12345612345678"
+    };
+    let payment = Payment {
+        payee: &restaurant,
+        amount: 1000,
+        currency: "EUR",
+    };
+
+    assert_eq!(payment.payee.name(), "We make Pizza!");
+    assert_eq!(payment.payee.iban(), "GB99ABCD12345612345678");
+    assert_eq!(payment.amount, 1000);
+    assert_eq!(payment.currency, "EUR");
+
+    // When the payment is in a different currency
+    {
+        let payment = Payment {
+            payee: &restaurant,
+            amount: 2000,
+            currency: "GBP",
+        };
+
+        assert_eq!(payment.payee.name(), "We make Pizza!");
+        assert_eq!(payment.payee.iban(), "GB99ABCD12345612345678");
+        assert_eq!(payment.amount, 2000);
+        assert_eq!(payment.currency, "GBP");
+    }
+
+    // The original variable is back in scope now
+    assert_eq!(payment.amount, 1000);
+    assert_eq!(payment.currency, "EUR");
+}
