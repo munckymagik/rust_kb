7d7d2842e8f1fc41e5bb0e1b33df2f9d8affd978
Dan Munckton <dan@cultivatehq.com>
2018-03-31 11:45:01 +0100

    Add an example of assert_ok as a function

--- tests/reducing_duplication.rs
+++ tests/reducing_duplication.rs
@@ -1,4 +1,5 @@
 use std::panic;
+use std::fmt::Display;
 
 macro_rules! assert_ok {
     ($e:expr) => (match $e {
@@ -7,11 +8,23 @@ macro_rules! assert_ok {
     })
 }
 
+fn fn_assert_ok<T, E: Display>(actual: Result<T, E>) {
+  match actual {
+      Ok(_)  => (),
+      Err(e) => panic!("Expected ok but was Err with: {}", e),
+  }
+}
+
 fn return_ok() -> Result<i32, i32> { Ok(1) }
 fn return_err() -> Result<i32, &'static str> { Err("Oh no!") }
 
 #[test]
 fn assert_ok_examples() {
+  fn_assert_ok(return_ok());
+  // fn_assert_ok(return_err());
+  // ---- assert_ok_examples stdout ----
+  // 	thread 'assert_ok_examples' panicked at 'Expected ok but was Err with: Oh no!', tests/reducing_duplication.rs:14:17
+
   assert_ok!(return_ok());
   // assert_ok!(return_err());
   // ---- assert_ok_examples stdout ----
@@ -28,7 +41,7 @@ fn assert_panic_unwind_example() {
     Err(ref boxed_any) => {
       match boxed_any.downcast_ref::<String>() {
         Some(cause) => assert_eq!(cause, "assert_ok!(return_err()) failed with: Oh no!"),
-        None        => panic!("Cause is not a String")
+        None        => panic!("Cause is not a String"),
       }
     }
   }
