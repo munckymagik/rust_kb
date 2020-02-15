342733b9874c8bc63309ba146e5bfa463d7d29cd
Dan Munckton <munckfish@gmail.com>
2020-02-15 21:29:52 +0000

    byte_str: implement eq_ignore_ascii_case and Display

--- standard/tests/byte_str.rs
+++ standard/tests/byte_str.rs
@@ -6,6 +6,7 @@
 // * https://users.rust-lang.org/t/how-to-create-a-slice-like-type/29073/3
 
 use std::borrow::Cow;
+use std::fmt::{self, Display};
 
 #[derive(Debug, PartialEq)]
 struct BStr<'inner> {
@@ -23,6 +24,13 @@ impl<'outer> BStr<'outer> {
     fn to_str_lossy(&self) -> Cow<str> {
         String::from_utf8_lossy(self.bytes)
     }
+
+    fn eq_ignore_ascii_case<T>(&self, other: T) -> bool
+    where
+        T: AsRef<[u8]>
+    {
+        self.bytes.eq_ignore_ascii_case(other.as_ref())
+    }
 }
 
 impl<'outer, T> From<&'outer T> for BStr<'outer>
@@ -31,6 +39,18 @@ where
 {
     fn from(other: &'outer T) -> Self {
         BStr { bytes: other.as_ref() }
+    }
+}
+
+impl AsRef<[u8]> for BStr<'_> {
+    fn as_ref(&self) -> &[u8] {
+        self.bytes
+    }
+}
+
+impl Display for BStr<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", self.to_str_lossy())
     }
 }
 
@@ -57,4 +77,26 @@ fn test_bstr_from() {
 fn test_bstr_to_str_lossy() {
     let a = BStr::from("hello");
     assert_eq!(a.to_str_lossy(), "hello");
+}
+
+#[test]
+fn test_bstr_display() {
+    use std::fmt::Write;
+
+    let a = BStr::from("hello");
+    let mut buf = String::new();
+
+    write!(buf, "{}", a).unwrap();
+
+    assert_eq!(buf, "hello")
+}
+
+#[test]
+fn test_bstr_eq_ignore_ascii_case() {
+    let a = BStr::from("hEllo");
+    let b = BStr::from("helLo");
+
+    assert!(a.eq_ignore_ascii_case(&b));
+    assert!(a.eq_ignore_ascii_case(b"HeLlO"));
+    assert!(a.eq_ignore_ascii_case("HeLlO"));
 }
