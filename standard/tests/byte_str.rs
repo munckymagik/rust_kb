7dab87316e01a0504f2a46d2b70f4486d897f4ad
Dan Munckton <munckfish@gmail.com>
2020-02-15 15:21:00 +0000

    byte_str: add to_str_lossy

--- standard/tests/byte_str.rs
+++ standard/tests/byte_str.rs
@@ -1,6 +1,11 @@
-// Experimenting with alternatives for a BStr e.g.
-// https://github.com/BurntSushi/bstr/blob/master/src/bstr.rs
-// See https://docs.rs/bstr/0.2.11/bstr/fn.B.html for why we need as_ref()
+// Experimenting with safe-only alternatives for a BStr e.g.
+//
+// Useful links:
+// * https://github.com/BurntSushi/bstr/blob/master/src/bstr.rs
+// * https://docs.rs/bstr/0.2.11/bstr/fn.B.html for why we need as_ref()
+// * https://users.rust-lang.org/t/how-to-create-a-slice-like-type/29073/3
+
+use std::borrow::Cow;
 
 #[derive(Debug, PartialEq)]
 struct BStr<'inner> {
@@ -14,6 +19,10 @@ impl<'outer> BStr<'outer> {
     {
         BStr { bytes: other.as_ref() }
     }
+
+    fn to_str_lossy(&self) -> Cow<str> {
+        String::from_utf8_lossy(self.bytes)
+    }
 }
 
 impl<'outer, T> From<&'outer T> for BStr<'outer>
@@ -26,7 +35,12 @@ where
 }
 
 #[test]
-fn test_bstr() {
+fn test_bstr_size() {
+    assert_eq!(std::mem::size_of::<&[u8]>(), 16);
+    assert_eq!(std::mem::size_of::<BStr>(), 16);
+}
+#[test]
+fn test_bstr_new() {
     let a = BStr::new(b"hello");
     let b = BStr::new("hello");
     assert_eq!(a, b);
@@ -37,4 +51,10 @@ fn test_bstr_from() {
     let a = BStr::from(b"hello");
     let b = BStr::from("hello");
     assert_eq!(a, b);
+}
+
+#[test]
+fn test_bstr_to_str_lossy() {
+    let a = BStr::from("hello");
+    assert_eq!(a.to_str_lossy(), "hello");
 }
