fe6ccdbd9245bd5d404c8b0faced08c4c3909c64
Dan Munckton <munckfish@gmail.com>
2021-03-20 11:10:42 +0000

    other one

--- perf/src/main.rs
+++ perf/src/main.rs
@@ -1,7 +1,5 @@
-use std::{
-    // mem::{size_of, size_of_val},
-    time::Instant,
-};
+#![allow(dead_code)]
+use std::{time::{Duration, Instant}, vec};
 
 struct Matrix {
     rows: usize,
@@ -55,14 +53,52 @@ fn sum_matrix(m: &Matrix, row_major: bool) -> i64 {
     sum
 }
 
+fn bench_strides() {
+    println!("Allocating 128MiB array ...");
+    let mut arr: Vec<i32> = vec![1; (1024 * 1024 * 128) / 4];
+    println!("Allocated 128MiB array. Benching ...");
+
+    // For each stride
+    for j in &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 32, 64, 128, 256, 512, 1024] {
+        // Repeat 20 times
+        for _ in 0..10 {
+            let start = Instant::now();
+
+            // Striding through the array
+            for i in (0..arr.len()).step_by(*j) {
+                arr[i] = arr[i].wrapping_mul(3);
+            }
+            println!("{}\t{}", j, (Instant::now() - start).as_millis())
+        }
+    }
+}
+
 fn time_it<F, T>(label: &str, f: F) -> T
 where
     F: Fn() -> T,
+    T: Default
 {
-    let start = Instant::now();
-    let result: T = f();
-    let d = Instant::now() - start;
-    println!("[{}] duration: {}us", label, d.as_micros());
+    let mut result: T = T::default();
+    let mut stats = Vec::with_capacity(1024);
+
+    for _ in 0..50 {
+        let start = Instant::now();
+        result = f();
+        stats.push(Instant::now() - start);
+    }
+
+    stats.sort();
+    let p50 = if stats.len() % 2 == 0 {
+        let upper = stats[stats.len() / 2];
+        let lower = stats[(stats.len() / 2) - 1];
+        (lower + upper) / 2
+    } else {
+        stats[stats.len() / 2]
+    };
+    let avg: Duration = stats.iter().sum::<Duration>() / stats.len() as u32;
+
+    println!("[{}] p50 duration: {}us", label, p50.as_micros());
+    println!("[{}] avg duration: {}us", label, avg.as_micros());
     result
 }
 
@@ -82,9 +118,13 @@ fn show_sizes(bytes: usize) {
     println!("% of L3 = {}%", (bytes as f64 / L3_SIZE as f64) * 100.0);
 }
 
-fn main() {
+fn matrix_demo() {
     let m = Matrix::new(L3_LINES, LINE_SIZE);
     show_sizes(m.memsize());
     println!("sum: {}", time_it("row major", || sum_matrix(&m, true)));
     println!("sum: {}", time_it("col major", || sum_matrix(&m, false)));
+}
+
+fn main() {
+    bench_strides();
 }
